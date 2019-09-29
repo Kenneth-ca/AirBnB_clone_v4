08c02be418a3ded67eb1e0514ec1e422c4a584c6
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-29 21:02:21 +0000

    [Feature] {Untested} Added script to check the amenities in a list and apply filters

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,2 +1,11 @@
-$('document').ready( function () {
-  $('INPUT'
+$('document').ready(function () {
+  const aList = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).prop('checked')) {
+      aList[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete aList[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(aList).join(', '));
+  });
+});
